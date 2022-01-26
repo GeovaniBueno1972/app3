7f036319fe2ee958536c0327da46218c418c9810
GeovaniBueno1972 <you@example.comgeovani.bueno@yahoo.com.br>
2022-01-26 18:55:04 -0300

    Alteração 26/01/22

--- app3/src/components/elementos/TabelaClientes.jsx
+++ app3/src/components/elementos/TabelaClientes.jsx
@@ -1,30 +1,28 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
-import * as locales from '@mui/material/locale';
-
-
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import { createTheme, ThemeProvider, useTheme } from "@mui/material/styles";
+import * as locales from "@mui/material/locale";
 
 export default function TabelaClientes(props) {
-    const theme = useTheme();
+  const theme = useTheme();
 
-    const themeWithLocale = React.useMemo(
-        () => createTheme(theme, locales['ptBR'])
-      );
+  const themeWithLocale = React.useMemo(() =>
+    createTheme(theme, locales["ptBR"])
+  );
 
-    const columns = [
-        { id: "id", label: 'Código', minWidth: 20 },
-        { id: "name", label: 'Nome', minWidth: 100 },
-        { id: "fone", label: 'Fone', minWidth: 170},
-        { id: "bairro", label: 'Bairro', minWidth: 170}
-    ]
+  const columns = [
+    { id: "id", label: "Código", minWidth: 20 },
+    { id: "name", label: "Nome", minWidth: 100 },
+    { id: "fone", label: "Fone", minWidth: 170 },
+    { id: "bairro", label: "Bairro", minWidth: 170 },
+  ];
 
   const [page, setPagina] = React.useState(0);
   const [rowsPerPage, setLinhasPorPagina] = React.useState(10);
@@ -38,56 +36,51 @@ export default function TabelaClientes(props) {
     setPagina(0);
   };
 
-  const rows = props.clientes
-  console.log('Linhas' + rows);
+  const rows = props.clientes;
+  console.log("Linhas" + rows);
 
   return (
-    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
-        <ThemeProvider theme={themeWithLocale}>
-      <TableContainer sx={{ maxHeight: 440 }}>
-        <Table stickyHeader aria-label="sticky table">
-          <TableHead>
-            <TableRow>
-              {columns.map((column) => (
-                <TableCell
-                  key={column.id}
-                  
-                  style={{ minWidth: column.minWidth }}
-                >
-                  {column.label}
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {rows
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((row) => {
-                return (
-                  <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
-                    {columns.map((column) => {
-                      const value = row[column.id];
-                      return (
-                        <TableCell key={column.id} >
-                          {value}
-                        </TableCell>
-                      );
-                    })}
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={props.clientes.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePagina}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <ThemeProvider theme={themeWithLocale}>
+        <TableContainer sx={{ maxHeight: 440 }}>
+          <Table stickyHeader aria-label="sticky table">
+            <TableHead>
+              <TableRow>
+                {columns.map((column) => (
+                  <TableCell
+                    key={column.id}
+                    style={{ minWidth: column.minWidth }}
+                  >
+                    {column.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row) => {
+                  return (
+                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
+                      {columns.map((column) => {
+                        const value = row[column.id];
+                        return <TableCell key={column.id}>{value}</TableCell>;
+                      })}
+                    </TableRow>
+                  );
+                })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[10, 25, 100]}
+          component="div"
+          count={props.clientes.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePagina}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
       </ThemeProvider>
     </Paper>
   );
